694929a8112903ccd3a4aa20bc3877b64c1bc00e
Алексей Макеев <117520389+MaboriHome@users.noreply.github.com>
2023-01-11 17:01:02 +0300

    feat: create reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
   
+  let str = String(n).split('');
+
+  if (str[0] === '-') {
+    str = str.slice(1);
+  }
+
+  for (let i = 0, j = str.length - 1; i < str.length / 2; i++, j--) {
+
+    [str[i], str[j]] = [str[j], str[i]];
+  }
+  return Number(str.join(''));
 }
